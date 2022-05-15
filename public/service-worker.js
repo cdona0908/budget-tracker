f51f1f1a61ef0b2cab46b6a7260e57dfa71e77b3
Celia <celiamdona@gmail.com>
2022-05-15 15:16:06 -0400

    service worker activation and retrieving info from cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,7 +19,7 @@ const APP_PREFIX = 'budget-tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-
+//install the service worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -29,3 +29,41 @@ self.addEventListener('install', function (e) {
     )
 
 })
+
+// activate service worker and remove any old data from the cache
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+});
+
+//Retrieve information from the cache by intercepting fetch requests
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+              console.log('responding with cache : ' + e.request.url)
+              return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })  
+    )
+  })
